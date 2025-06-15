c4097aaa9d063961df26c8d8aaf3b876c0a64604
Safayet <hossainsafayet187@gmail.com>
2025-06-15 10:00:00 +0600

    items and installment

--- 7.php
+++ 7.php
@@ -10,7 +10,7 @@ abstract class OurClass{
 class MyClass extends OurClass{
 
     public function greet(){
-        echo "hello from abstract class";
+        echo "hello from abstract classes";
     }
 
 }
