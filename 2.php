0b40795ab0af0b363778529e595779e655e01948
Safayet <hossainsafayet187@gmail.com>
2025-11-04 00:08:54 +0600

    public private

---
+++ 2.php
@@ -0,0 +1,29 @@
+<?php
+
+class Fund{
+
+    private $fund;
+
+    function __construct($initialFund = 0){
+     $this->fund = $initialFund;   
+    }
+    public function addFund($money){
+        $this->fund += $money;
+
+    }
+
+    public function deductFund($money){
+        $this->fund -= $money;
+    }
+
+    public function getTotal(){
+        echo "Total fund is {$this->fund}\n";
+    }
+}
+
+$ourFund = new Fund(100);
+$ourFund->getTotal();
+$ourFund->addFund(10);
+$ourFund->getTotal();
+$ourFund->deductFund(20);
+$ourFund->getTotal();
