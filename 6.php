84170709573f6b02ba48083da31e9757183d9fcc
Safayet <hossainsafayet187@gmail.com>
2025-11-06 01:25:20 +0600

    abstract

---
+++ 6.php
@@ -0,0 +1,35 @@
+<?php
+
+abstract class Shape{
+    abstract function getArea();
+
+    abstract function getPerimeter();
+
+    
+}
+
+class Rectangle extends Shape{
+    
+    private $base,$height;
+
+
+    public function __construct($base,$height)
+    {
+        $this->base = $base;
+        $this->height = $height;
+    }
+    public function setBase($base){
+        $this->base = $base;
+    }  public function setHeight($height){
+        $this->base = $height;
+    }
+
+    function getArea(){
+        return $this->base * $this->height;
+    }
+
+    function getPerimeter()
+    {
+        
+    }
+}
