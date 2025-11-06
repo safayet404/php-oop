8d6b9f56be167072beb71fa0b8132ca204c951e9
Safayet <hossainsafayet187@gmail.com>
2025-11-07 00:15:09 +0600

    abstract function from parent class

--- 7.php
+++ 7.php
@@ -4,10 +4,17 @@ abstract class OurClass{
     final function doSomething(){
         echo "Doing Something";
     }
+
+    abstract function greet();
 }
 class MyClass extends OurClass{
+
+    public function greet(){
+        echo "hello from abstract class";
+    }
 
 }
 
 $mc = new MyClass();
 $mc->doSomething();
+$mc->greet();
