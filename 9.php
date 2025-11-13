9b43b2e3dc098ddca11af218fe8be327611b758f
Safayet <hossainsafayet187@gmail.com>
2025-11-13 23:42:45 +0600

    interface

---
+++ 9.php
@@ -0,0 +1,51 @@
+<?php 
+
+interface BaseAnimal {
+    function isAlive();
+    function canEat($param1,$param2);
+    function breed();
+}
+
+
+class Animal implements BaseAnimal{
+    function isAlive()
+    {
+       echo "Yes ,this animal is alive";
+    }
+
+    function canEat($param1,$param2)
+    {
+        echo "Can eat bhat and Fish";
+    }
+
+    function breed()
+    {
+        echo "This is Persian cat";
+    }
+}
+
+interface BaseHuman extends BaseAnimal{
+    public function canTalk();
+}
+
+class Human implements BaseHuman{
+    function isAlive()
+    {
+       echo "Yes ,this animal is alive";
+    }
+
+    function canEat($param1,$param2)
+    {
+        echo "Can eat everything literally";
+    }
+
+    function breed()
+    {
+        echo "Finest Breed";
+    }
+
+    function canTalk()
+    {
+        echo "Hello World!";
+    }
+}
